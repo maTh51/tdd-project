152c471ed4f232e1c9b0650bba3cb6460edca5c5
maTh51 <matheustigon@gmail.com>
2022-02-02 20:46:33 -0300

    Não Salvar Items em Branco a cada Requisição

--- lists/tests.py
+++ lists/tests.py
@@ -33,3 +33,7 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
         self.assertEqual(second_saved_item.text, 'O segundo item')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
