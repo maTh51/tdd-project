e28702282d4f10c62107edb2e2124067e9021754
maTh51 <matheustigon@gmail.com>
2022-02-02 21:47:08 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 5

--- lists/tests.py
+++ lists/tests.py
@@ -23,10 +23,6 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
         self.assertEqual(second_saved_item.text, 'O segundo item')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
     
 
 class ListViewTest(TestCase):
