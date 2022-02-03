3abea731a0e8730b34d78c7301d5b6690e8f74fe
maTh51 <matheustigon@gmail.com>
2022-02-02 23:12:04 -0300

    Entrada inválida não deve ser salva no BD

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -55,6 +55,13 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+    
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
 
 class NewItemTest(TestCase):
@@ -85,9 +92,3 @@ class NewItemTest(TestCase):
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
     
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
